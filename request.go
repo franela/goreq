46f69765cc81ff9b5bccbf8c56cfb5b4749b6b30
Jonathan Leibiusky <ionathan@gmail.com>
2013-10-29 16:17:56 -0400

    add support for connect timeout and request timeout

--- request.go
+++ request.go
@@ -7,12 +7,15 @@ import (
     "io"
     "encoding/json"
     "bytes"
+    "time"
+    "net"
 )
 
 type Request struct {
     Method string
     Uri string
     Body interface{}
+    Timeout time.Duration
 }
 
 type Response struct {
@@ -22,7 +25,14 @@ type Response struct {
 }
 
 type Error struct {
+    requestTimeout bool
+}
 
+func (e *Error) ConnectTimeout() (bool) {
+    return !e.requestTimeout
+}
+func (e *Error) RequestTimeout() (bool) {
+    return e.requestTimeout
 }
 
 func prepareRequestBody(b interface{}) (io.Reader) {
@@ -46,18 +56,39 @@ func prepareRequestBody(b interface{}) (io.Reader) {
    return body
 }
 
-func newResponse(res *http.Response) (Response) {
+func newResponse(res *http.Response) (*Response) {
     body, _ := ioutil.ReadAll(res.Body)
     // TODO: handle error
-    return Response{ StatusCode: res.StatusCode, Header: res.Header, Body: string(body) }
+    return &Response{ StatusCode: res.StatusCode, Header: res.Header, Body: string(body) }
 }
 
-func (r Request) Do() (Response, *Error) {
-    client := &http.Client{}
+var dialer = &net.Dialer{ Timeout: 1000 * time.Millisecond }
+var transport = &http.Transport{ Dial: dialer.Dial }
+
+func SetConnectTimeout(duration time.Duration) {
+    dialer.Timeout = duration
+}
+
+func (r Request) Do() (*Response, *Error) {
+    client := &http.Client{ Transport: transport }
     b := prepareRequestBody(r.Body)
     req, _ := http.NewRequest(r.Method, r.Uri, b)
     // TODO: handler error
-    res, _ := client.Do(req)
+    requestTimeout := false
+    var timer *time.Timer
+    if r.Timeout > 0 {
+        timer = time.AfterFunc(r.Timeout, func() {
+            transport.CancelRequest(req)
+            requestTimeout = true
+        })
+    }
+    res, err := client.Do(req)
+    if timer != nil {
+        timer.Stop()
+    }
     // TODO: handler error
+    if err != nil {
+        return nil, &Error{ requestTimeout: requestTimeout }
+    }
     return newResponse(res), nil
 }
